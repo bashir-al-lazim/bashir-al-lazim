2f320c70f8f33e78f21de98881b4de83d8ab2b67
MD. Bashir Al Lazim <md.bashirallazim@gmail.com>
2025-01-01 23:57:00 +0600

    basic setup

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <h1 className="text-5xl font-bold text-center">
+      Hey, I am Bashir! I am actively looking for Junior MERN Stack positions.
+    </h1>
+  </StrictMode>,
+)
