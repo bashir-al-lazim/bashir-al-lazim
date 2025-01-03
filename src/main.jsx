c625e9213fc990cbe81c79b0210035caeadcc1fc
MD. Bashir Al Lazim <md.bashirallazim@gmail.com>
2025-01-03 23:00:46 +0600

    router setup done

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { RouterProvider } from "react-router-dom";
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <h1 className="text-5xl font-bold text-center">
-      Hey, I am Bashir! I am actively looking for Junior MERN Stack positions.
-    </h1>
+    <RouterProvider router={router} />
   </StrictMode>,
 )
